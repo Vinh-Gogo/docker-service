adad78173d581a9d71ec78859a4a489130a45692
vinh-gogo <lea26462@gmail.com>
2025-11-25 13:35:21 +0700

    add model 8B

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # docker-service
 
 ![alt text](image.png)
+
+
+```bash
+docker model pull ai/qwen3:4B-UD-Q4_K_XL
+``
--- dowload-models.py
+++ dowload-models.py
@@ -4,7 +4,7 @@ os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "0"
 
 from huggingface_hub import snapshot_download
 snapshot_download(
-  repo_id = "BAAI/bge-reranker-v2-m3",
-  local_dir = "./models/bge-reranker-v2-m3",
-#   allow_patterns = ["*Q6_K_XL*"],
+  repo_id = "Qwen/Qwen3-8B-GGUF",
+  local_dir = "./models/Qwen3-8B.gguf-Q8_0.gguf",
+  allow_patterns = ["*-Q8_0.gguf*"],
 )
