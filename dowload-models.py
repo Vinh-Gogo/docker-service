6b3247d9320aa1cd2f25dc98b91c6f55214df370
vinh-gogo <lea26462@gmail.com>
2026-03-16 18:44:02 +0700

    add two models

--- dowload-models.py
+++ dowload-models.py
@@ -1,30 +1,11 @@
-# # download model from huggingface
-# import os # Optional for faster downloading
-# from huggingface_hub import snapshot_download
-
-# os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "0"
-
-# snapshot_download(
-#   repo_id = "Qwen/Qwen3-Embedding-0.6B",
-#   local_dir = "./models/Qwen3-Embedding-0.6B",
-#   # allow_patterns = ["*-Q8_0.gguf*"],
-# )
-
 # !pip install huggingface_hub hf_transfer
 # download model from huggingface
 import os # Optional for faster downloading
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
-# from huggingface_hub import snapshot_download
-# snapshot_download(
-#   repo_id = "unsloth/Qwen3-30B-A3B-GGUF",
-#   local_dir = "unsloth_Qwen3-30B-A3B-GGUF",
-#   allow_patterns = ["*Q6_K_XL*"],
-# )
-
 from huggingface_hub import snapshot_download
 snapshot_download(
-  repo_id = "unsloth/Qwen3.5-35B-A3B-GGUF",
-  local_dir = "unsloth/Qwen3.5-35B-A3B-GGUF",
-  allow_patterns = ["*UD-IQ4_NL*"],
+  repo_id = "mradermacher/Huihui-Qwen3.5-27B-Claude-4.6-Opus-abliterated-i1-GGUF",
+  local_dir = "models/Huihui-Qwen3.5-27B-Claude-4.6-Opus-abliterated-i1-GGUF",
+  allow_patterns = ["*Q4_K_S*"],
 )
